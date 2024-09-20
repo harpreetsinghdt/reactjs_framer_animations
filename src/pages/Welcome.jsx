1484488df59392414855e03ab32c4e60df905527
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-09-20 16:12:34 -0400

    background color check around

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -13,9 +13,16 @@ export default function WelcomePage() {
   );
   const yCity = useTransform(scrollY, [0, 200], [0, -100]);
   const opacityHero = useTransform(scrollY, [0, 300, 500], [1, 1, 0]);
+
   const yHero = useTransform(scrollY, [0, 200], [0, -150]);
   const yText = useTransform(scrollY, [0, 200, 300, 500], [0, 50, 50, 300]);
+
   const scaleText = useTransform(scrollY, [0, 300], [1, 1.5]);
+  // const backgroundColor = useTransform(
+  //   scrollY,
+  //   [0, 200, 300, 500],
+  //   ["#eee", "#eeddee", "#ddeedd", "#00f"]
+  // );
   return (
     <>
       <header id="welcome-header">
