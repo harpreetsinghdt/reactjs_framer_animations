a532f0ed3859b9068f1dcc8fa13600b25af437a6
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-09-21 11:30:19 -0400

    updates

---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <main id="welcome-content">
+      <section>
+        <h2>There&apos;s never been a better time.</h2>
+        <p>
+          With our platform, you can set, track, and conquer challenges at your
+          own pace. Whether it&apos;s personal growth, professional
+          achievements, or just for fun, we&apos;ve got you covered.
+        </p>
+      </section>
+    </main>
+  );
+};
+
+export default About;
